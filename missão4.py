2eb83ccf3f76496cb7eef0494cd412b2fa38ad44
thaisbfr1 <thbfr1@gmail.com>
2026-08-25 19:38:23 -0300

    Add files via upload

---
+++ missão4.py
@@ -0,0 +1,9 @@
+nota = float(input("digite a nota do aluno de 0 a 10"))
+if nota >= 9:
+    print("conceito A - execelnte")
+if nota >= 7:
+    print("conceito b - bom") 
+if nota >= 5:
+    print("conceito c - regular")
+else:
+   print ("conceito D reprovado")
