e1b4009147a3e60c8cbad0a486f0a2b380c4a730
thaisbfr1 <thbfr1@gmail.com>
2026-08-25 19:37:25 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,11 @@
+idade = int(input("digite sua idade"))
+altura = float(input("digite sua altura"))
+
+if idade >= 10 and altura >= 1.40:
+    print("pode entrar no brinquedo radical!")
+else:
+    print ("não pode entrar no brinquedo")   
+if idade <10:
+    print ("motivo: idade miniima é 10 anos")   
+if altura < 1.40:   
+    print ("motivo: altura minima é 1.40")     
