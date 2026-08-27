6d33d07f9c39f52fc097d01d4160fc45e8ae06dd
thaisbfr1 <thbfr1@gmail.com>
2026-08-27 18:41:23 -0300

    Add files via upload

---
+++ aula4.py/aula.3.py
@@ -0,0 +1,6 @@
+tem_ingresso = input("voce tem ingresso")
+aniversariante = input("você é aniversariante ? sim ou não")
+if tem_ingresso or aniversariante :
+    print("entrada liberada")
+else:
+    print ("precisa comprar ingresso")
